b600d24dba75defa423cc0f890c32090c4927dde
Stanislav <stanislav.kachurovskyi@vmedia.ca>
2023-12-06 19:15:49 +0200

    docker filessd

--- src/main/java/com/example/greenbluetest/TestController.java
+++ src/main/java/com/example/greenbluetest/TestController.java
@@ -11,7 +11,7 @@ public class TestController {
 
     @GetMapping("/")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("v1.4");
+        return ResponseEntity.ok("v1.5");
     }
 
 }
